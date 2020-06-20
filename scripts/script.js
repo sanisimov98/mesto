82fc422a702768a1504bb8adad447c82547de191
aleksandranisimov <sanisimov98@gmail.com>
2020-06-20 22:31:32 +0300

    правки

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
-let popup = document.querySelector('.popup');
-let popupElOpen = document.querySelector('.profile__edit-button');
-let popupElClose = document.querySelector('.popup__form-close');
-let profileName = document.querySelector('.profile__title');
-let profileText = document.querySelector('.profile__text');
-let formName = document.querySelector('.popup__form-item_el_name');
+let popup = document.querySelector('.popup'); // всплывающее окно
+let popupElOpen = document.querySelector('.profile__edit-button'); // кнопка правки
+let popupElClose = document.querySelector('.popup__form-close'); // кнопка закрытия всплывающего окна
+let profileName = document.querySelector('.profile__title'); // имя пользователя
+let profileText = document.querySelector('.profile__text'); // текст профиля
+let formName = document.querySelector('.popup__form-item_el_name'); // редактирования во в
 let formText = document.querySelector('.popup__form-item_el_text');
 let formElement = document.querySelector('.popup');
 let formButton = document.querySelector('.popup__form-button');
@@ -21,11 +21,9 @@ function popupHide() {
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    let nameInput = document.querySelector('.popup__form-item_el_name'); 
-    let jobInput = document.querySelector('.popup__form-item_el_text'); 
-
-    document.querySelector('.profile__title').textContent = nameInput.value;
-    document.querySelector('.profile__text').textContent = jobInput.value;
+    
+    document.querySelector('.profile__title').textContent = formName.value;
+    document.querySelector('.profile__text').textContent = formText.value;
 
     popupHide()
 }
